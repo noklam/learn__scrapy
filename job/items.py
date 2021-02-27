706cda3f58b61766ca2facecb4eb64365e2bea18
noklam <channoklam@yahoo.com.hk>
2021-02-27 19:34:12 +0800

    parse url success

--- job/items.py
+++ job/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class JobItem(scrapy.Item):
+class JobPagination(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     url = scrapy.Field()
--- job/spiders/jobsdb.py
+++ job/spiders/jobsdb.py
@@ -1,7 +1,8 @@
 import scrapy
 from scrapy.spiders import CrawlSpider
-from scrapy.spiders import Rules
+from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
+from ..items import JobPagination
 
 # class StationDetailSpider(CrawlSpider):
 #     name = 'train'
@@ -12,16 +13,29 @@ from scrapy.linkextractors import LinkExtractor
 #     )
 #     def parse_trains(self, response):
 #         '''do your parsing here''
+"div[data-automation='pagination-dropdown']+a::attr(href)"
 
-
-class JobsdbSpider(scrapy.Spider):
+class JobsdbSpider(CrawlSpider):
     name = "jobsdb"
-    allowed_domains = ["hk.josbdb.com"]
-    start_urls = ["http://hk.josbdb.com/"]
+    allowed_domains = ["hk.jobsdb.com"]
+    start_urls = ["https://hk.jobsdb.com/hk/search-jobs/data-scientist/1"]
     header = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
     }
     max_page = 10
+    rules = (
+        Rule(
+            LinkExtractor(
+                restrict_css=("div[data-automation='pagination-dropdown']+a",),
+                attrs=("href",),
+            ),
+            follow=True,
+            callback="parse_page",
+        ),
+    )
 
-    def parse(self, response):
-        pass
+    def parse_page(self, response):
+        o = JobPagination()
+        o["url"] = response.url
+        print("************NOK****************", o)
+        yield o
